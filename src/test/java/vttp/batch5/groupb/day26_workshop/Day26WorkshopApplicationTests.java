2434d26091a52c700e346b4c97bc6eb204fbe8da
Aiken Ong <aiken.ong@u.nus.edu>
2025-02-11 09:12:47 +0800

    initial commit

---
+++ src/test/java/vttp/batch5/groupb/day26_workshop/Day26WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp.batch5.groupb.day26_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day26WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
